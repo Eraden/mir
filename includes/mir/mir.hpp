a0cff0843e5946cd51ed2941a093973a83807874
Adrian Wozniak <adrian.wozniak@ita-prog.pl>
2019-04-13 21:03:10 +0200

    Add include all

---
+++ includes/mir/mir.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <mir/db/Connection.hpp>
+#include <mir/db/Query.hpp>
+#include <mir/db/Url.hpp>
+
+#include <mir/mir/CommandLineInterface.hpp>
+#include <mir/mir/ConfigReader.hpp>
+#include <mir/mir/DownMigrator.hpp>
+#include <mir/mir/Generator.hpp>
+#include <mir/mir/LoadMigrationsQuery.hpp>
+#include <mir/mir/MigrationDirectoryReader.hpp>
+#include <mir/mir/RunMigrationQuery.hpp>
+#include <mir/mir/SetupMigrator.hpp>
+#include <mir/mir/UpMigrator.hpp>
+
+#include <mir/utils/Logger.hpp>
+
